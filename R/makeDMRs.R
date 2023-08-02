70d0a58367c0a64f2a65acdc536b958de3c9d085
Simon Pearce <simon.pearce@cruk.manchester.ac.uk>
2023-08-02 10:13:21 +0100

    minCounts on filtered samples

--- R/makeDMRs.R
+++ R/makeDMRs.R
@@ -88,7 +88,7 @@ fitQseaGLM <- function(qseaSet, variable = NULL,  covariates = NULL,
   }
 
   if (is.null(keepIndex) & minNRPM == 0 ) {
-    keepIndex = which(matrixStats::rowMaxs(qsea::getCounts(qseaSet)) >= minReadCount)
+    keepIndex = which(matrixStats::rowMaxs(qsea::getCounts(qseaSet %>% filter(sample_name %in% samplesInContrasts))) >= minReadCount)
   }
 
   if (is.null(keepIndex) & minNRPM >= 0) {
