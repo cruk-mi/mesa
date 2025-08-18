a7169180865cbf443fd4e3ad42294ac360d254d2
fperezmartinez <felipe.perezmartinez@manchester.ac.uk>
2025-08-18 13:09:02 +0100

    docs(utils-pipe.R): Added examples

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -8,4 +8,13 @@
 #' @export
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
+#' 
+#' @examples
+#' # Basic usage: pipe a vector into sum()
+#' 1:5 %>% sum()
+#'
+#' # Chain multiple transformations
+#' 1:10 %>%
+#'   mean() %>%
+#'   round(1)
 NULL
