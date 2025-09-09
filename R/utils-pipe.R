7fa331016458986972c27b9b3cfd8b60cd39e534
fperezmartinez <felipe.perezmartinez@manchester.ac.uk>
2025-09-09 16:51:29 +0100

    docs(R/utils-pipe.R): harmonized function doc format, and updated examples to use pipe syntax

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -1,16 +1,20 @@
 #' Pipe operator
 #'
-#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#' Re-export of the magrittr pipe. See
+#' \code{\link[magrittr:pipe]{\%>\%}} for full details.
+#' It forwards the left-hand side (LHS) as the first argument to the
+#' right-hand side (RHS) call.
 #'
 #' @name %>%
 #' @rdname pipe
-#' @keywords internal
-#' @export
-#' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
-#' 
+#' @return The result of applying \code{rhs} to \code{lhs}.
+#' @keywords internal
+#' @importFrom magrittr %>%
+#' @export
+#'
 #' @examples
-#' # Basic usage: pipe a vector into sum()
+#' # Basic usage
 #' 1:5 %>% sum()
 #'
 #' # Chain multiple transformations
