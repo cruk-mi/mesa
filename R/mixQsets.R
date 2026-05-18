bb8cb7665b9158a0929442fd8e729d965b915325
fpmartinez10 <fpmartinez10@gmail.com>
2026-05-18 13:02:24 +0100

    style(R/mixQsets.R): wrap lines > 80 chars to satisfy BiocCheck note

--- R/mixQsets.R
+++ R/mixQsets.R
@@ -99,9 +99,13 @@
 #'     sum()
 #'
 #' @export
-mixSamples <- function(qseaSet, sample1, sample2, nReadsTotal, proportion, newName = NULL, groupName = NULL,
+mixSamples <- function(
+    qseaSet, sample1, sample2,
+    nReadsTotal, proportion,
+    newName = NULL, groupName = NULL,
     onlyNew = FALSE,
-    renormalise = TRUE) {
+    renormalise = TRUE
+) {
     qsea:::checkSamples(qseaSet, c(sample1, sample2))
 
     if (is.null(newName)) {
@@ -118,36 +122,61 @@ mixSamples <- function(qseaSet, sample1, sample2, nReadsTotal, proportion, newNa
     nReads2 <- nReadsTotal - nReads1
 
     if (proportion < 0 || proportion > 1) {
-        stop(glue::glue("Proportion must be a number between 0 and 1, not {proportion}."))
+        stop(glue::glue(
+            "Proportion must be a number between 0 and 1,",
+            " not {proportion}."
+        ))
     }
 
-    validFragNums <- qseaSet@libraries$file_name[c(sample1, sample2), "valid_fragments", drop = FALSE] %>%
+    validFragNums <- qseaSet@libraries$file_name[
+        c(sample1, sample2), "valid_fragments", drop = FALSE
+    ] %>%
         tibble::rownames_to_column() %>%
         tibble::deframe()
 
     counts <- qseaSet@count_matrix[, c(sample1, sample2)]
     sampleColSums <- colSums(counts)
 
-    message(glue::glue("Mixing {nReads1} reads from {sample1} with {nReads2} reads from {sample2}."))
+    message(glue::glue(
+        "Mixing {nReads1} reads from {sample1}",
+        " with {nReads2} reads from {sample2}."
+    ))
 
     onTargetFrac1 <- sampleColSums[sample1] / validFragNums[sample1]
     onTargetFrac2 <- sampleColSums[sample2] / validFragNums[sample2]
 
     if (sampleColSums[sample1] < nReads1 * onTargetFrac1) {
-        stop(glue::glue("Not enough reads in {sample1}, only {sampleColSums[sample1]} out of {nReads1 * onTargetFrac1} requested."))
+        stop(glue::glue(
+            "Not enough reads in {sample1},",
+            " only {sampleColSums[sample1]} out of",
+            " {nReads1 * onTargetFrac1} requested."
+        ))
     }
 
     if (sampleColSums[sample2] < nReads2 * onTargetFrac2) {
-        stop(glue::glue("Not enough reads in {sample2}, only {sampleColSums[sample2]} out of {nReads2 * onTargetFrac2} requested."))
+        stop(glue::glue(
+            "Not enough reads in {sample2},",
+            " only {sampleColSums[sample2]} out of",
+            " {nReads2 * onTargetFrac2} requested."
+        ))
     }
 
-    windows1 <- sample(rep(seq_len(nrow(counts)), counts[, sample1]), replace = FALSE, size = nReads1 * onTargetFrac1)
-    windows2 <- sample(rep(seq_len(nrow(counts)), counts[, sample2]), replace = FALSE, size = nReads2 * onTargetFrac2)
+    windows1 <- sample(
+        rep(seq_len(nrow(counts)), counts[, sample1]),
+        replace = FALSE, size = nReads1 * onTargetFrac1
+    )
+    windows2 <- sample(
+        rep(seq_len(nrow(counts)), counts[, sample2]),
+        replace = FALSE, size = nReads2 * onTargetFrac2
+    )
 
     newCounts <- c(windows1, windows2) %>%
         tibble::enframe(name = NULL, value = "window") %>%
         dplyr::count(window, name = "n") %>%
-        dplyr::left_join(tibble::tibble(window = seq_len(nrow(counts))), ., copy = TRUE, by = "window") %>%
+        dplyr::left_join(
+            tibble::tibble(window = seq_len(nrow(counts))),
+            ., copy = TRUE, by = "window"
+        ) %>%
         dplyr::mutate(n = tidyr::replace_na(n, 0)) %>%
         dplyr::select(n)
 
@@ -163,10 +192,14 @@ mixSamples <- function(qseaSet, sample1, sample2, nReadsTotal, proportion, newNa
         GenomicRanges::mcols() %>%
         as.matrix()
 
-    GenomicRanges::mcols(newSet@cnv) <- proportion * reducedCNVMat[, sample1] + (1 - proportion) * reducedCNVMat[, sample2]
+    GenomicRanges::mcols(newSet@cnv) <-
+        proportion * reducedCNVMat[, sample1] +
+        (1 - proportion) * reducedCNVMat[, sample2]
     colnames(GenomicRanges::mcols(newSet@cnv)) <- newName
 
-    newSet@zygosity <- proportion * qseaSet@zygosity[sample1, , drop = FALSE] + (1 - proportion) * qseaSet@zygosity[sample2, , drop = FALSE]
+    newSet@zygosity <-
+        proportion * qseaSet@zygosity[sample1, , drop = FALSE] +
+        (1 - proportion) * qseaSet@zygosity[sample2, , drop = FALSE]
     rownames(newSet@zygosity) <- newName
 
     newSet@sampleTable <- tibble::tibble(
@@ -302,13 +335,20 @@ mixSamples <- function(qseaSet, sample1, sample2, nReadsTotal, proportion, newNa
 #' }
 #'
 #' @keywords internal
-mixThreeQsetSamples <- function(qseaSet, sample1, sample2, sample3, nReadsTotal, proportion1, proportion2, newName = NULL, groupName = NULL,
-                                onlyNew = FALSE,
-                                renormalise = TRUE) {
+mixThreeQsetSamples <- function(
+    qseaSet, sample1, sample2, sample3,
+    nReadsTotal, proportion1, proportion2,
+    newName = NULL, groupName = NULL,
+    onlyNew = FALSE,
+    renormalise = TRUE
+) {
     qsea:::checkSamples(qseaSet, c(sample1, sample2, sample3))
 
     if (is.null(newName)) {
-        newName <- paste0("Mix", "_", sample1, "_", sample2, "_", sample3, "_", proportion1, "_", proportion2)
+        newName <- paste0(
+            "Mix", "_", sample1, "_", sample2, "_",
+            sample3, "_", proportion1, "_", proportion2
+        )
     }
 
     message(newName)
@@ -322,26 +362,41 @@ mixThreeQsetSamples <- function(qseaSet, sample1, sample2, sample3, nReadsTotal,
     nReads3 <- nReadsTotal - nReads1 - nReads2
 
     if (proportion1 < 0 || proportion1 > 1) {
-        stop(glue::glue("Proportion1 must be a number between 0 and 1, not {proportion1}."))
+        stop(glue::glue(
+            "Proportion1 must be a number between 0 and 1,",
+            " not {proportion1}."
+        ))
     }
 
     if (proportion2 < 0 || proportion2 > 1) {
-        stop(glue::glue("Proportion2 must be a number between 0 and 1, not {proportion2}."))
+        stop(glue::glue(
+            "Proportion2 must be a number between 0 and 1,",
+            " not {proportion2}."
+        ))
     }
 
 
     if (proportion1 + proportion2 >= 1) {
-        stop(glue::glue("Sum of proportions must be less than 1, not {proportion1 + proportion2}."))
+        stop(glue::glue(
+            "Sum of proportions must be less than 1,",
+            " not {proportion1 + proportion2}."
+        ))
     }
 
-    validFragNums <- qseaSet@libraries$file_name[c(sample1, sample2, sample3), "valid_fragments", drop = FALSE] %>%
+    validFragNums <- qseaSet@libraries$file_name[
+        c(sample1, sample2, sample3), "valid_fragments", drop = FALSE
+    ] %>%
         tibble::rownames_to_column() %>%
         tibble::deframe()
 
     counts <- qseaSet@count_matrix[, c(sample1, sample2, sample3)]
     sampleColSums <- colSums(counts)
 
-    message(glue::glue("Mixing {nReads1} reads from {sample1} with {nReads2} reads from {sample2} and {nReads3} reads from {sample3}."))
+    message(glue::glue(
+        "Mixing {nReads1} reads from {sample1}",
+        " with {nReads2} reads from {sample2}",
+        " and {nReads3} reads from {sample3}."
+    ))
 
     nReads1 <- ceiling(nReadsTotal * proportion1)
     nReads2 <- ceiling(nReadsTotal * proportion2)
@@ -352,26 +407,50 @@ mixThreeQsetSamples <- function(qseaSet, sample1, sample2, sample3, nReadsTotal,
     onTargetFrac3 <- sampleColSums[sample3] / validFragNums[sample3]
 
     if (sampleColSums[sample1] < nReads1 * onTargetFrac1) {
-        stop(glue::glue("Not enough reads in {sample1}, only {sampleColSums[sample1]} out of {nReads1 * onTargetFrac1} requested."))
+        stop(glue::glue(
+            "Not enough reads in {sample1},",
+            " only {sampleColSums[sample1]} out of",
+            " {nReads1 * onTargetFrac1} requested."
+        ))
     }
 
     if (sampleColSums[sample2] < nReads2 * onTargetFrac2) {
-        stop(glue::glue("Not enough reads in {sample2}, only {sampleColSums[sample2]} out of {nReads2 * onTargetFrac2} requested."))
+        stop(glue::glue(
+            "Not enough reads in {sample2},",
+            " only {sampleColSums[sample2]} out of",
+            " {nReads2 * onTargetFrac2} requested."
+        ))
     }
 
     if (sampleColSums[sample3] < nReads3 * onTargetFrac2) {
-        stop(glue::glue("Not enough reads in {sample3}, only {sampleColSums[sample3]} out of {nReads3 * onTargetFrac3} requested."))
+        stop(glue::glue(
+            "Not enough reads in {sample3},",
+            " only {sampleColSums[sample3]} out of",
+            " {nReads3 * onTargetFrac3} requested."
+        ))
     }
 
-    windows1 <- sample(rep(seq_len(nrow(counts)), counts[, sample1]), replace = FALSE, size = nReads1 * onTargetFrac1)
-    windows2 <- sample(rep(seq_len(nrow(counts)), counts[, sample2]), replace = FALSE, size = nReads2 * onTargetFrac2)
-    windows3 <- sample(rep(seq_len(nrow(counts)), counts[, sample3]), replace = FALSE, size = nReads3 * onTargetFrac3)
+    windows1 <- sample(
+        rep(seq_len(nrow(counts)), counts[, sample1]),
+        replace = FALSE, size = nReads1 * onTargetFrac1
+    )
+    windows2 <- sample(
+        rep(seq_len(nrow(counts)), counts[, sample2]),
+        replace = FALSE, size = nReads2 * onTargetFrac2
+    )
+    windows3 <- sample(
+        rep(seq_len(nrow(counts)), counts[, sample3]),
+        replace = FALSE, size = nReads3 * onTargetFrac3
+    )
 
     newCounts <- c(windows1, windows2, windows3) %>%
         table() %>%
         tibble::enframe(name = "window") %>%
         dplyr::mutate(window = as.integer(window)) %>%
-        dplyr::left_join(tibble::tibble(window = seq_len(nrow(counts))), ., copy = TRUE, by = "window") %>%
+        dplyr::left_join(
+            tibble::tibble(window = seq_len(nrow(counts))),
+            ., copy = TRUE, by = "window"
+        ) %>%
         dplyr::mutate(value = tidyr::replace_na(value, 0)) %>%
         dplyr::select(value)
 
@@ -387,10 +466,17 @@ mixThreeQsetSamples <- function(qseaSet, sample1, sample2, sample3, nReadsTotal,
         GenomicRanges::mcols() %>%
         as.matrix()
 
-    GenomicRanges::mcols(newSet@cnv) <- proportion1 * reducedCNVMat[, sample1] + proportion2 * reducedCNVMat[, sample2] + (1 - proportion1 - proportion2) * reducedCNVMat[, sample3]
+    GenomicRanges::mcols(newSet@cnv) <-
+        proportion1 * reducedCNVMat[, sample1] +
+        proportion2 * reducedCNVMat[, sample2] +
+        (1 - proportion1 - proportion2) * reducedCNVMat[, sample3]
     colnames(GenomicRanges::mcols(newSet@cnv)) <- newName
 
-    newSet@zygosity <- proportion1 * qseaSet@zygosity[sample1, , drop = FALSE] + proportion2 * qseaSet@zygosity[sample2, , drop = FALSE] + (1 - proportion1 - proportion2) * qseaSet@zygosity[sample3, , drop = FALSE]
+    newSet@zygosity <-
+        proportion1 * qseaSet@zygosity[sample1, , drop = FALSE] +
+        proportion2 * qseaSet@zygosity[sample2, , drop = FALSE] +
+        (1 - proportion1 - proportion2) *
+        qseaSet@zygosity[sample3, , drop = FALSE]
     rownames(newSet@zygosity) <- newName
 
     newSet@sampleTable <- tibble::tibble(
