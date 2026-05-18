b86ce1c33506d37552c45e6bd7e3ec24daaeb7c8
fpmartinez10 <fpmartinez10@gmail.com>
2026-05-18 11:23:22 +0100

    style(R/analyseDMRs.R): wrap lines > 80 chars to satisfy BiocCheck note

--- R/analyseDMRs.R
+++ R/analyseDMRs.R
@@ -32,29 +32,41 @@
 #'
 #' # Convert results to long format and filter at custom FDR threshold
 #' exampleTumourNormal %>%
-#'     calculateDMRs(variable = "type",
+#'     calculateDMRs(
+#'         variable = "type",
 #'         contrasts = "LUAD_vs_NormalLung",
-#'         FDRthres = 0.1) %>%
+#'         FDRthres = 0.1
+#'     ) %>%
 #'     pivotDMRsLonger(FDRthres = 0.1)
 #'
 #' @export
 pivotDMRsLonger <- function(DMRtable, FDRthres = 0.05, makePositive = FALSE) {
     pivotedDMRs <- DMRtable %>%
-        dplyr::rename_with(~ stringr::str_replace(.x, "_adjPval", ":adjPval")) %>%
-        dplyr::rename_with(~ stringr::str_replace(.x, "_log2FC", ":log2FC")) %>%
-        dplyr::rename_with(~ stringr::str_replace(.x, "_deltaBeta", ":deltaBeta")) %>%
+        dplyr::rename_with(
+            ~ stringr::str_replace(.x, "_adjPval", ":adjPval")
+        ) %>%
+        dplyr::rename_with(
+            ~ stringr::str_replace(.x, "_log2FC", ":log2FC")
+        ) %>%
+        dplyr::rename_with(
+            ~ stringr::str_replace(.x, "_deltaBeta", ":deltaBeta")
+        ) %>%
         tidyr::pivot_longer(tidyselect::matches("_vs_"),
             names_to = c(".comparison", ".ext"),
             values_to = ".value",
-            names_sep = ":") %>%
-        tidyr::pivot_wider(names_from = .ext,
-            values_from = .value) %>%
+            names_sep = ":"
+        ) %>%
+        tidyr::pivot_wider(
+            names_from = .ext,
+            values_from = .value
+        ) %>%
         dplyr::filter(adjPval <= FDRthres) %>%
         tidyr::separate(.comparison, into = c("group1", "group2"), sep = "_vs_")
 
     if (makePositive) {
         pivotedDMRs <- pivotedDMRs %>%
-            dplyr::mutate(group1new = ifelse(log2FC < 0, group2, group1),
+            dplyr::mutate(
+                group1new = ifelse(log2FC < 0, group2, group1),
                 group2new = ifelse(log2FC < 0, group1, group2),
                 deltaBeta = sign(log2FC) * deltaBeta,
                 log2FC = sign(log2FC) * log2FC,
@@ -106,27 +118,31 @@ pivotDMRsLonger <- function(DMRtable, FDRthres = 0.05, makePositive = FALSE) {
 #'
 #' # Summarise DMRs for a single contrast, using custom FDR threshold
 #' exampleTumourNormal %>%
-#'     calculateDMRs(variable = "type",
+#'     calculateDMRs(
+#'         variable = "type",
 #'         contrasts = "LUAD_vs_NormalLung",
-#'         FDRthres = 0.1) %>%
+#'         FDRthres = 0.1
+#'     ) %>%
 #'     summariseDMRsByContrast(FDRthres = 0.1)
 #'
 #' @export
-summariseDMRsByContrast <- function(DMRtable, FDRthres = 0.05, log2FCthres = 0, deltaBetaThres = 0) {
-
+summariseDMRsByContrast <- function(
+    DMRtable, FDRthres = 0.05, log2FCthres = 0, deltaBetaThres = 0
+) {
     if (!("adjPval" %in% colnames(DMRtable))) {
-
         contrastsDF <- DMRtable %>%
             colnames() %>%
             stringr::str_subset(".*_vs_.*_adjPval$") %>%
             tibble::enframe(name = NULL) %>%
-            tidyr::separate(value, into = c("group1", "group2", NA), sep = "_vs_|_adjPval") %>%
+            tidyr::separate(
+                value, into = c("group1", "group2", NA),
+                sep = "_vs_|_adjPval"
+            ) %>%
             dplyr::arrange(group1, group2)
 
         DMRtable <- DMRtable %>%
             dplyr::select(-tidyselect::matches("_nrpm$|_beta$|_means$")) %>%
             pivotDMRsLonger(FDRthres = FDRthres)
-
     } else {
         contrastsDF <- DMRtable %>%
             dplyr::distinct(group1, group2) %>%
@@ -135,7 +151,8 @@ summariseDMRsByContrast <- function(DMRtable, FDRthres = 0.05, log2FCthres = 0, 
 
     DMRsummary <- DMRtable %>%
         tibble::as_tibble() %>%
-        dplyr::filter(adjPval <= FDRthres,
+        dplyr::filter(
+            adjPval <= FDRthres,
             abs(log2FC) >= log2FCthres,
             abs(deltaBeta) >= deltaBetaThres,
         ) %>%
@@ -147,8 +164,10 @@ summariseDMRsByContrast <- function(DMRtable, FDRthres = 0.05, log2FCthres = 0, 
 
     contrastsDF %>%
         dplyr::left_join(DMRsummary, by = dplyr::join_by(group1, group2)) %>%
-        dplyr::mutate(nDown = tidyr::replace_na(nDown, 0),
-            nUp = tidyr::replace_na(nUp, 0)) %>%
+        dplyr::mutate(
+            nDown = tidyr::replace_na(nDown, 0),
+            nUp = tidyr::replace_na(nUp, 0)
+        ) %>%
         dplyr::select(group1, group2, nUp, nDown) %>%
         return()
 }
@@ -174,21 +193,23 @@ summariseDMRsByContrast <- function(DMRtable, FDRthres = 0.05, log2FCthres = 0, 
 #' @family DMR-helpers
 #'
 #' @examples
-#' \donttest{
 #' data(exampleTumourNormal, package = "mesa")
 #'
 #' # Summarise DMRs with explicit annotation databases
 #' exampleTumourNormal %>%
 #'     calculateDMRs(variable = "tumour", contrasts = "all") %>%
-#'     annotateWindows(TxDb = "TxDb.Hsapiens.UCSC.hg38.knownGene",
-#'         annoDb = "org.Hs.eg.db") %>%
+#'     annotateWindows(
+#'         TxDb = "TxDb.Hsapiens.UCSC.hg38.knownGene",
+#'         annoDb = "org.Hs.eg.db"
+#'     ) %>%
 #'     summariseDMRsByGene()
-#' }
 #' @export
 summariseDMRsByGene <- function(DMRtable) {
-
     if (!("ENSEMBL" %in% colnames(DMRtable))) {
-        stop("Please call annotateWindows first on the DMRs prior to summariseDMRsByGene.")
+        stop(
+            "Please call annotateWindows first on the",
+            " DMRs prior to summariseDMRsByGene."
+        )
     }
 
     summary <- DMRtable %>%
@@ -199,7 +220,6 @@ summariseDMRsByGene <- function(DMRtable) {
         dplyr::arrange(dplyr::desc(n))
 
     return(summary)
-
 }
 
 
@@ -236,7 +256,6 @@ summariseDMRsByGene <- function(DMRtable) {
 #' @family DMR-helpers
 #'
 #' @examples
-#' \donttest{
 #' data(exampleTumourNormal, package = "mesa")
 #'
 #' # Minimal example: write results for a single contrast
@@ -249,10 +268,8 @@ summariseDMRsByGene <- function(DMRtable) {
 #'     calculateDMRs(variable = "tumour", contrasts = "all") %>%
 #'     annotateWindows(TxDb = "TxDb.Hsapiens.UCSC.hg38.knownGene", annoDb = "org.Hs.eg.db") %>%
 #'     writeDMRsToExcel(path = file.path(tempdir(), "test.xlsx"))
-#' }
 #' @export
 writeDMRsToExcel <- function(dataTable, path, FDRthres = 0.05) {
-
     if (!requireNamespace("openxlsx", quietly = TRUE)) {
         stop(
             "Package \"openxlsx\" must be installed to use this function.",
@@ -262,10 +279,11 @@ writeDMRsToExcel <- function(dataTable, path, FDRthres = 0.05) {
 
     wb_DMR <- openxlsx::createWorkbook()
 
-    contrasts <- dataTable %>% colnames() %>% stringr::str_subset("adjPval$")
+    contrasts <- dataTable %>%
+        colnames() %>%
+        stringr::str_subset("adjPval$")
 
     purrr::walk(contrasts, function(x) {
-
         ## TODO Make this cleaner, particularly for multiple tabs to stop them being the same name
         ## Probably by using walk2 and the name.
         sheetName <- stringr::str_trunc(x, 31, "right", ellipsis = "")
@@ -274,11 +292,12 @@ writeDMRsToExcel <- function(dataTable, path, FDRthres = 0.05) {
         dataTable %>%
             dplyr::filter(!!dplyr::sym(x) <= FDRthres) %>%
             openxlsx::writeData(wb_DMR, sheetName, .)
-        openxlsx::addFilter(wb_DMR, sheetName, row = 1, cols = seq_len(ncol(dataTable)))
-        openxlsx::freezePane(wb_DMR, sheetName,  firstRow = TRUE)
-
-    }
-    )
+        openxlsx::addFilter(
+            wb_DMR, sheetName, row = 1,
+            cols = seq_len(ncol(dataTable))
+        )
+        openxlsx::freezePane(wb_DMR, sheetName, firstRow = TRUE)
+    })
 
     openxlsx::saveWorkbook(wb_DMR, file = path, overwrite = TRUE)
     invisible(dataTable)
@@ -312,44 +331,49 @@ writeDMRsToExcel <- function(dataTable, path, FDRthres = 0.05) {
 #' @family DMR-helpers
 #'
 #' @examples
-#' \donttest{
 #' data(exampleTumourNormal, package = "mesa")
 #'
 #' # Export DMRs for a single contrast to BED files in a temp directory
 #' exampleTumourNormal %>%
-#'     calculateDMRs(variable = "type",
-#'         contrasts = "LUAD_vs_NormalLung") %>%
+#'     calculateDMRs(
+#'         variable = "type",
+#'         contrasts = "LUAD_vs_NormalLung"
+#'     ) %>%
 #'     writeDMRsToBed(folder = tempdir())
 #'
 #' # Export DMRs with explicit annotation (requires TxDb/annotation packages)
 #' exampleTumourNormal %>%
 #'     calculateDMRs(variable = "tumour", contrasts = "all") %>%
-#'     annotateWindows(TxDb = "TxDb.Hsapiens.UCSC.hg38.knownGene",
-#'         annoDb = "org.Hs.eg.db") %>%
+#'     annotateWindows(
+#'         TxDb = "TxDb.Hsapiens.UCSC.hg38.knownGene",
+#'         annoDb = "org.Hs.eg.db"
+#'     ) %>%
 #'     writeDMRsToBed(folder = tempdir(), FDRthres = 0.1)
-#' }
 #' @export
 writeDMRsToBed <- function(dataTable, folder, FDRthres = 0.05) {
-
     dir.create(folder, showWarnings = FALSE, recursive = TRUE)
 
-    contrasts <- dataTable %>% colnames() %>% stringr::str_subset("adjPval$")
+    contrasts <- dataTable %>%
+        colnames() %>%
+        stringr::str_subset("adjPval$")
 
     if (length(contrasts) == 0) {
-        stop("No columns ending in adjPval found! Is this a set of DMRs generated by calculateDMRs?")
+        stop(
+            "No columns ending in adjPval found!",
+            " Is this a set of DMRs generated by calculateDMRs?"
+        )
     }
 
     purrr::walk(contrasts, function(x) {
-
         contrastName <- stringr::str_remove(x, "_adjPval$")
 
         dataTable %>%
             dplyr::filter(!!rlang::sym(x) <= !!FDRthres) %>%
             plyranges::as_granges() %>%
-            rtracklayer::export.bed(file.path(folder, paste0(contrastName, ".bed")))
-
-    }
-    )
+            rtracklayer::export.bed(
+                file.path(folder, paste0(contrastName, ".bed"))
+            )
+    })
 
     invisible(dataTable)
 }
@@ -366,8 +390,10 @@ writeDMRsToBed <- function(dataTable, folder, FDRthres = 0.05) {
 #' @examples
 #' # calculate some DMRs
 #' DMRs <- exampleTumourNormal %>%
-#'     calculateDMRs(variable = "type", contrasts = "all",
-#'         keepContrastMeans = FALSE)
+#'     calculateDMRs(
+#'         variable = "type", contrasts = "all",
+#'         keepContrastMeans = FALSE
+#'     )
 #' # Find the DMRs with the largest deltaBeta between each comparison:
 #' DMRs %>% sliceDMRs(n = 1)
 #' # Or the windows with the largest log2FC:
@@ -379,8 +405,9 @@ writeDMRsToBed <- function(dataTable, folder, FDRthres = 0.05) {
 #' # If position is used, then the windows are sorted by genomic position:
 #' DMRs %>% sliceDMRs(n = 1, metric = position)
 #' @export
-sliceDMRs <- function(DMRs, n = 1, metric = deltaBeta, makePositive = TRUE, FDRthres = 0.05) {
-
+sliceDMRs <- function(
+    DMRs, n = 1, metric = deltaBeta, makePositive = TRUE, FDRthres = 0.05
+) {
     n <- as.integer(n)
     if (length(n) != 1L || is.na(n) || n < 0L) {
         stop("`n` must be a single non-negative integer.")
@@ -388,7 +415,10 @@ sliceDMRs <- function(DMRs, n = 1, metric = deltaBeta, makePositive = TRUE, FDRt
 
     positiveDMRs <- DMRs %>%
         pivotDMRsLonger(makePositive = makePositive, FDRthres = FDRthres) %>%
-        dplyr::relocate(tidyselect::matches("means$"), .after = tidyselect::last_col()) %>%
+        dplyr::relocate(
+            tidyselect::matches("means$"),
+            .after = tidyselect::last_col()
+        ) %>%
         dplyr::group_by(group1, group2)
 
     if (deparse(substitute(metric)) %in% c("position")) {
@@ -397,7 +427,10 @@ sliceDMRs <- function(DMRs, n = 1, metric = deltaBeta, makePositive = TRUE, FDRt
             dplyr::slice_head(n = n) %>%
             dplyr::ungroup()
     } else if (deparse(substitute(metric)) %in% c("adjPval")) {
-        message(glue::glue("Choosing the windows with the smallest value of {rlang::ensym(metric)}"))
+        message(glue::glue(
+            "Choosing the windows with the smallest",
+            " value of {rlang::ensym(metric)}"
+        ))
         out <- positiveDMRs %>%
             dplyr::arrange({{ metric }}) %>%
             dplyr::slice_head(n = n) %>%
